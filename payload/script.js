70345b100c23272e244a21c80d804f3da47b942e
Restyled.io <commits@restyled.io>
2021-04-14 22:30:12 +0000

    Restyled by clang-format

--- payload/script.js
+++ payload/script.js
@@ -1,10 +1,8 @@
-document.onmousemove = function (event) {
-	window.moveBy(event.clientX - 150, event.clientY - 115);
-	window.resizeTo(300, 300);
+document.onmousemove = function(event) {
+  window.moveBy(event.clientX - 150, event.clientY - 115);
+  window.resizeTo(300, 300);
 
-	/* window.moveTo(event.clientX - 125, event.clientY - 100); */
+  /* window.moveTo(event.clientX - 125, event.clientY - 100); */
 };
 
-window.onbeforeunload = function () {
-	localStorage.setItem("panic", true);
-};
+window.onbeforeunload = function() { localStorage.setItem("panic", true); };
--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 document.onmousemove = function listener(e) {
-	var thiswindow = window.open(
-		"/payload",
-		"NO ESCAPE",
-		`toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=300,height=300`
-	);
-	thiswindow.moveTo(e.clientX - 150, e.clientY - 100);
+  var thiswindow = window.open(
+      "/payload", "NO ESCAPE",
+      `toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=300,height=300`);
+  thiswindow.moveTo(e.clientX - 150, e.clientY - 100);
   window.location.href = "../rick";
 };
