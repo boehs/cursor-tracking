683f976c5e8ab5454596044444bf51932e600576
Restyled.io <commits@restyled.io>
2021-04-14 22:30:29 +0000

    Restyled by prettier

--- payload/script.js
+++ payload/script.js
@@ -1,8 +1,10 @@
-document.onmousemove = function(event) {
+document.onmousemove = function (event) {
   window.moveBy(event.clientX - 150, event.clientY - 115);
   window.resizeTo(300, 300);
 
   /* window.moveTo(event.clientX - 125, event.clientY - 100); */
 };
 
-window.onbeforeunload = function() { localStorage.setItem("panic", true); };
+window.onbeforeunload = function () {
+  localStorage.setItem("panic", true);
+};
--- rick/script.js
+++ rick/script.js
@@ -4,8 +4,10 @@ document.onmousemove = function listener(e) {
   let panic = localStorage.getItem("panic");
   if (panic || panic == null) {
     var thiswindow = window.open(
-        "/payload", "NO ESCAPE",
-        `toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=300,height=300`);
+      "/payload",
+      "NO ESCAPE",
+      `toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=300,height=300`
+    );
     localStorage.setItem("panic", false);
     thiswindow.moveTo(e.clientX - 150, e.clientY - 100);
   }
@@ -13,5 +15,6 @@ document.onmousemove = function listener(e) {
   var y = e.clientY;
 };
 
-window.addEventListener("load",
-                        (event) => { localStorage.setItem("panic", true); });
+window.addEventListener("load", (event) => {
+  localStorage.setItem("panic", true);
+});
