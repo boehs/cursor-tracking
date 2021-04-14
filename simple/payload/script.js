581a24cb016230378e914f92332064fed5a16bc0
Evan Boehs <ebrobot2000@gmail.com>
2021-04-14 08:28:02 -0400

    Update script.js

--- simple/payload/script.js
+++ simple/payload/script.js
@@ -2,5 +2,5 @@ document.onmousemove = function (event) {
   	window.moveBy(event.clientX - 125, event.clientY - 100);
 	window.resizeTo(300, 300);
 	
-	/* window.moveTo(event.clientX - 150, event.clientY - 100); */
+	/* window.moveTo(event.clientX - 125, event.clientY - 100); */
 };
