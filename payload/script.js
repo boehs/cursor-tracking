d16a2be9e91d3d46b7f8d925010ef0901f4d67b0
Evan Boehs <ebrobot2000@gmail.com>
2021-04-14 18:21:34 -0400

    lol

--- payload/script.js
+++ payload/script.js
@@ -1,6 +1,10 @@
 document.onmousemove = function (event) {
-  	window.moveBy(event.clientX - 150, event.clientY - 115);
+	window.moveBy(event.clientX - 150, event.clientY - 115);
 	window.resizeTo(300, 300);
-	
+
 	/* window.moveTo(event.clientX - 125, event.clientY - 100); */
+};
+
+window.onbeforeunload = function () {
+	localStorage.setItem("panic", true);
 };
