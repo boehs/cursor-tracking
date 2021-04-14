a05e1cf18f0ef41c4a61b315dcd5fafe5c71f5fd
Evan Boehs <ebrobot2000@gmail.com>
2021-04-14 08:27:52 -0400

    Update script.js

--- simple/payload/script.js
+++ simple/payload/script.js
@@ -1,5 +1,5 @@
 document.onmousemove = function (event) {
-  	window.moveBy(event.clientX - 150, event.clientY - 100);
+  	window.moveBy(event.clientX - 125, event.clientY - 100);
 	window.resizeTo(300, 300);
 	
 	/* window.moveTo(event.clientX - 150, event.clientY - 100); */
